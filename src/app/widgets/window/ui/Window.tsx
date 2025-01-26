e10db9a58e88a7c3233af709c9a5974fd9293345
username <ivlevaarina@gmail.com>
2025-01-26 12:56:07 +0600

    ui

--- src/app/widgets/window/ui/Window.tsx
+++ src/app/widgets/window/ui/Window.tsx
@@ -5,28 +5,45 @@ export const Window: React.FC<WindowProps> = props => {
 	const { form_header } = { ...props }
 	const { contact_label } = { ...props }
 
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		console.log('0')
+	}
+
 	return (
-		<div className={style.window}>
-			<p className={style.form_header}>{form_header}</p>
-			<div className={style.text_input}>
-				<input type='text' placeholder='Name' className={style.input} />
-				<input type='email' placeholder='Email' className={style.input} />
-				<input type='tel' placeholder='Phone' className={style.input} />
+		<form onSubmit={onSubmit}>
+			<div className={style.window}>
+				<p className={style.form_header}>{form_header}</p>
+				<div className={style.text_input}>
+					<input type='text' placeholder='Name' className={style.input} />
+					<input type='email' placeholder='Email' className={style.input} />
+					<input type='tel' placeholder='Phone' className={style.input} />
+				</div>
+				<label htmlFor='contact_method'>{contact_label}</label>
+				<div className={style.contact_method}>
+					<div>
+						<input type='radio' name='method' id='0' />
+						<label htmlFor='Email'> Email</label>
+					</div>
+					<div>
+						<input type='radio' name='method' id='1' />
+						<label htmlFor='Phone'> Phone</label>
+					</div>
+				</div>
+				<div className={style.textarea}>
+					<div>
+						<input type='checkbox' name='agree' />
+						<label htmlFor='agree'> I agree to receive a reply by my email</label>
+					</div>
+					<textarea
+						name='massage'
+						placeholder='Message'
+						rows={6}
+						className={style.input}
+					></textarea>
+				</div>
+				<input type='submit' value='Submit' className={style.submit} />
 			</div>
-			<label htmlFor='contact_method'>{contact_label}</label>
-			<div className={style.contact_method}>
-				<input type='radio' name='method' id='0' />
-				<label htmlFor='Email'>Email</label>
-				<input type='radio' name='method' id='1' />
-				<label htmlFor='Phone'>Phone</label>
-			</div>
-			<textarea
-				name='massage'
-				placeholder='Message'
-				rows={6}
-				className={style.input}
-			></textarea>
-      <input type="submit" value='Submit' className={style.submit}/>
-		</div>
+		</form>
 	)
 }
