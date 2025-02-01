ef4553de5dcc8e54d87ecf4b1563bee959547284
username <ivlevaarina@gmail.com>
2025-02-01 23:05:15 +0600

    +

--- src/app/widgets/window/ui/Window.tsx
+++ src/app/widgets/window/ui/Window.tsx
@@ -11,6 +11,7 @@ export const Window: React.FC<WindowProps> = props => {
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 	const [number, setNumber] = useState('')
+	const [contactMethod, setContactMethod] = useState('email')
 	const [message, setMessage] = useState('')
 
 	const nameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,6 +26,10 @@ export const Window: React.FC<WindowProps> = props => {
 		setNumber(e.target.value)
 	}
 
+	const contactMethodOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setContactMethod(e.target.value as 'email' | 'phone')
+	}
+
 	const messageOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setMessage(e.target.value)
 	}
@@ -34,7 +39,12 @@ export const Window: React.FC<WindowProps> = props => {
 		console.log(name)
 		console.log(email)
 		console.log(number)
+		console.log(contactMethod)
 		console.log(message)
+		setName('')
+		setEmail('')
+		setNumber('')
+		setContactMethod('')
 		setMessage('')
 	}
 
@@ -68,11 +78,25 @@ export const Window: React.FC<WindowProps> = props => {
 				<label htmlFor='contact_method'>{contact_label}</label>
 				<div className={style.contact_method}>
 					<div>
-						<input type='radio' name='method' id='0' />
+						<input
+							type='radio'
+							name='method'
+							id='contactMethodEmail'
+							value='email'
+							checked={contactMethod === 'email'}
+							onChange={contactMethodOnChange}
+						/>
 						<label htmlFor='Email'> Email</label>
 					</div>
 					<div>
-						<input type='radio' name='method' id='1' />
+						<input
+							type='radio'
+							name='method'
+							id='contactMethodPhone'
+							value='phone'
+							checked={contactMethod === 'phone'}
+							onChange={contactMethodOnChange}
+						/>
 						<label htmlFor='Phone'> Phone</label>
 					</div>
 				</div>
@@ -80,8 +104,7 @@ export const Window: React.FC<WindowProps> = props => {
 					<div>
 						<input type='checkbox' name='agree' />
 						<label htmlFor='agree'>
-							{' '}
-							I agree to receive a reply by my email
+							I agree to receive a reply by my {contactMethod}
 						</label>
 					</div>
 					<textarea
