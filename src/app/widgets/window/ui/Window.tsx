f128ad1a98e1a20702f852b04764a038c6f5086f
username <ivlevaarina@gmail.com>
2025-02-01 20:41:40 +0600

    setState

--- src/app/widgets/window/ui/Window.tsx
+++ src/app/widgets/window/ui/Window.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import React, { useState } from 'react'
 import { WindowProps } from '../model/types'
 import style from '../../../index.module.css'
 
@@ -5,9 +8,34 @@ export const Window: React.FC<WindowProps> = props => {
 	const { form_header } = { ...props }
 	const { contact_label } = { ...props }
 
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [number, setNumber] = useState('')
+	const [message, setMessage] = useState('')
+
+	const nameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setName(e.target.value)
+	}
+
+	const emailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEmail(e.target.value)
+	}
+
+	const numberOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNumber(e.target.value)
+	}
+
+	const messageOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setMessage(e.target.value)
+	}
+
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
-		console.log('0')
+		console.log(name)
+		console.log(email)
+		console.log(number)
+		console.log(message)
+		setMessage('')
 	}
 
 	return (
@@ -15,9 +43,27 @@ export const Window: React.FC<WindowProps> = props => {
 			<div className={style.window}>
 				<p className={style.form_header}>{form_header}</p>
 				<div className={style.text_input}>
-					<input type='text' placeholder='Name' className={style.input} />
-					<input type='email' placeholder='Email' className={style.input} />
-					<input type='tel' placeholder='Phone' className={style.input} />
+					<input
+						type='text'
+						placeholder='Name'
+						value={name}
+						onChange={nameOnChange}
+						className={style.input}
+					/>
+					<input
+						type='email'
+						placeholder='Email'
+						value={email}
+						onChange={emailOnChange}
+						className={style.input}
+					/>
+					<input
+						type='tel'
+						placeholder='Phone'
+						value={number}
+						onChange={numberOnChange}
+						className={style.input}
+					/>
 				</div>
 				<label htmlFor='contact_method'>{contact_label}</label>
 				<div className={style.contact_method}>
@@ -33,13 +79,18 @@ export const Window: React.FC<WindowProps> = props => {
 				<div className={style.textarea}>
 					<div>
 						<input type='checkbox' name='agree' />
-						<label htmlFor='agree'> I agree to receive a reply by my email</label>
+						<label htmlFor='agree'>
+							{' '}
+							I agree to receive a reply by my email
+						</label>
 					</div>
 					<textarea
 						name='massage'
 						placeholder='Message'
 						rows={6}
 						className={style.input}
+						value={message}
+						onChange={messageOnChange}
 					></textarea>
 				</div>
 				<input type='submit' value='Submit' className={style.submit} />
